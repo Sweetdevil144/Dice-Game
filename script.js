a9757c9e20f1b21e61ece4ad03417af7fa3615a9
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-03-09 21:59:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 function img1(randomNumber1) {
-
-    // if(randomNumber1 == 1) document.getElementsById("#img1").setAttribute("src", "/images/dice1.png");
-    // else if(randomNumber1 == 2) document.getElementsById("#img1").setAttribute("src", "/images/dice2.png");
-    // else if(randomNumber1 == 3) document.getElementsById("#img1").setAttribute("src", "/images/dice3.png");
-    // else if(randomNumber1 == 4) document.getElementsById("#img1").setAttribute("src", "/images/dice4.png");
-    // else if(randomNumber1 == 5) document.getElementsById("#img1").setAttribute("src", "/images/dice5.png");
-    // else if(randomNumber1 == 6) document.getElementsById("#img1").setAttribute("src", "/images/dice6.png");
+    var randonDiceImage = "images/dice" + randomNumber1 + ".png";
+    var setTo = document.getElementById("img1");
+    setTo.src = randonDiceImage;
 }
+
 function img2(randomNumber2) {
-    // if(randomNumber2 == 1) document.getElementsById("#img2").setAttribute("src", "/images/dice1.png");
-    // else if(randomNumber2 == 2) document.getElementsById("#img2").setAttribute("src", "/images/dice2.png");
-    // else if(randomNumber2 == 3) document.getElementsById("#img2").setAttribute("src", "/images/dice3.png");
-    // else if(randomNumber2 == 4) document.getElementsById("#img2").setAttribute("src", "/images/dice4.png");
-    // else if(randomNumber2 == 5) document.getElementsById("#img2").setAttribute("src", "/images/dice5.png");
-    // else if(randomNumber2 == 6) document.getElementsById("#img2").setAttribute("src", "/images/dice6.png");
+    var randonDiceImage = "images/dice" + randomNumber2 + ".png";
+    var setTo = document.getElementById("img2");
+    // setTo.setAttribute("src", randomDiceImage);
+    setTo.src = randonDiceImage;
 }
 
 function func() {
@@ -22,21 +17,24 @@ function func() {
     let randomNumber2 = Math.floor(Math.random()*6)+1;
     if(randomNumber1 > randomNumber2) {
         heading = "Player 1 wins";
-        console.log(heading);
-        document.getElementById("#result_print").textContent(heading);
+        // console.log(heading);
+        // document.getElementById("#result_print").textContent(heading);
+        document.querySelector("div #result_print").textContent = heading;
     }
     else if(randomNumber2>randomNumber1) {
         heading = "Player 2 wins";
-        console.log(heading);
-        document.getElementById("#result_print").textContent(heading);
+        // console.log(heading);
+        // document.getElementById("#result_print").textContent(heading);
+        document.querySelector("div #result_print").textContent = heading;
     }
     else {
         heading = "It's a Draw";
-        console.log(heading);
-        document.getElementById("#result_print").textContent(heading);
+        // console.log(heading);
+        // document.getElementById("#result_print").textContent(heading);
+        document.querySelector("div #result_print").textContent = heading;
     }
 
-    // img1(randomNumber1); img2(randomNumber2);
+    img1(randomNumber1); img2(randomNumber2);
 }
 
 
